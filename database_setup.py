18dcd799c6e55d25514101d8a23c0efebeb5eee0
Seho Lim <limseho657424@gmail.com>
2018-02-24 14:00:22 -0600

    added deployment queries. Finxed database setup.

--- database_setup.py
+++ database_setup.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, Boolean
 from sqlalchemy.orm import relationship
 
-
 Base = declarative_base()
 
 
@@ -12,69 +11,33 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String(250))
 
 
-class Course(Base):
-    __tablename__ = 'course'
+class Category(Base):
+    __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     description = Column(String(200))
     created_on = Column(DateTime, default=func.now())
     created_by = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    user = relationship(User)
-    cards = relationship('Card')
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializable format"""
-        card_data = [card.serialize for card in self.cards]
-        return {
-            'cards': card_data,
-            'description': self.description,
-            'name': self.name,
-            'id': self.id
-        }
+    user = relationship("User")
+    card = relationship("Card")
 
 
 class Card(Base):
     __tablename__ = 'card'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    description = Column(String, nullable=False)
-    memorized = Column(Integer)
+    name = Column(String(250), nullable=False)
+    translated_name = Column(String(250), nullable=False)
+    #memorized = Column(Integer)
     memorized_bool = Column(Boolean)
     created_on = Column(DateTime, default=func.now())
     created_by = Column(String(250), nullable=False)
-    course_id = Column(Integer, ForeignKey('course.id'))
-    course = relationship(Course)
+    category = relationship("Category")
+    category_id = Column(Integer, ForeignKey('category.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    memorized_cards = relationship('MemorizedCard')
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializable format"""
-        return {
-            'description': self.description,
-            'name': self.name,
-            'id': self.id
-        }
-
-
-class MemorizedCard(Base):
-    __tablename__ = 'memorized_cards'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    description = Column(String, nullable=False)
-    user = relationship(User)
-    course = relationship(Course)
-    card = relationship(Card)
-    created_on = Column(DateTime, default=func.now())
-    card_id = Column(Integer, ForeignKey('card.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    course_id = Column(Integer, ForeignKey('course.id'))
+    user = relationship("User")
 
 
 engine = create_engine('sqlite:///flashcard.db')
--- hello.py
+++ hello.py
@@ -1,17 +1,39 @@
 
 from flask import Flask
+from sqlalchemy import create_engine, asc, desc, and_
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Card
+
+
 app = Flask(__name__)
+app.secret_key = 'asdfasdf'
+app.debug = True
+
+engine = create_engine('sqlite:///flashcard.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+import new_catagory_handler
+import new_card_handler
 
 
-@app.route("/getFlashcards")
+@app.route("/populate")
+def populate():
+    session.add(user)
+    session.commit()
+    return "adsfasdfasdf"
+
+
+@app.route("/getUsers")
 def getstuff():
 
-    return "here are all the flashcards you need"
+    users_conn = session.query(User)
+    users = users_conn.all()
+    return "users"
 
 
 if __name__ == "__main__":
