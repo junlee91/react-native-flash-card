86f21fd80eb18004a04995213a2d6b5dae04688d
Seho Lim <limseho657424@gmail.com>
2018-02-24 14:55:03 -0600

    integrated hassan's code.

--- main_handler.py
+++ main_handler.py
@@ -18,7 +18,39 @@ session = DBSession()
 
 
 import new_catagory_handler
-import new_card_handler
+
+
+@app.route("/<int:category_id>/Card/new", methods=['GET', 'POST'])
+def new_Card(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    user = session.query(User).filter_by(id=get_user_id(login_session)).one()
+    if request.method == 'POST':
+        name = request.form['name']
+        category = request.form['category']
+        # Checks if the user input a name and a description.
+        if name and description:
+            cards = session.query(Card).filter_by(category_id=course.id)
+            name = name.lower()
+            for c in cards:
+                if c.name.lower() is name:
+                    flash("The %s card is already exists" %
+                          name)  # NEEDS TO BE DELETED
+                    return render_template('newCard.html', course=course,   # NEEDS TO BE REMOVED
+                                           description=description, name=name)
+                else:
+                    created_card = Card(name=name, description=description,
+                                        category_id=category_id, created_by=user.name,
+                                        user_id=user.id)
+                    session.add(created_card)
+                    session.commit()
+                    flash("Successfully created %s" % created_card.name)
+                    return redirect(url_for('show_cards', category_id=course.id))
+        else:
+            flash("Please input a name and a category")
+            return render_template('newCard.html', course=course,
+                                   description=description, name=name)
+    else:
+        return render_template('newCard.html', course=course)
 
 
 @app.route("/populate")
--- new_card_handler.py
+++ new_card_handler.py
@@ -1,4 +1,4 @@
-@app.route("/<int:category_id>/Card/new", methods=['GET', 'POST']) 
+@app.route("/<int:category_id>/Card/new", methods=['GET', 'POST'])
 def new_Card(category_id):
     category = session.query(Course).filter_by(id=category_id).one()
     user = session.query(User).filter_by(id=get_user_id(login_session)).one()
@@ -11,13 +11,14 @@ def new_Card(category_id):
             name = name.lower()
             for c in cards:
                 if c.name.lower() is name:
-                    flash("The %s card is already exists" % name) # NEEDS TO BE DELETED
+                    flash("The %s card is already exists" %
+                          name)  # NEEDS TO BE DELETED
                     return render_template('newCard.html', course=course,   # NEEDS TO BE REMOVED
-                                       description=description, name=name)
+                                           description=description, name=name)
                 else:
                     created_card = Card(name=name, description=description,
-                                    category_id=category_id, created_by=user.name,
-                                    user_id=user.id)
+                                        category_id=category_id, created_by=user.name,
+                                        user_id=user.id)
                     session.add(created_card)
                     session.commit()
                     flash("Successfully created %s" % created_card.name)
@@ -28,4 +29,3 @@ def new_Card(category_id):
                                    description=description, name=name)
     else:
         return render_template('newCard.html', course=course)
-     
