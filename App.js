effcdc97050f75e9944f9e2cbf95c3b95b646ead
junlee91 <jlee757@uic.edu>
2018-02-24 12:55:56 -0600

    Input Field

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 
 import AddComponent from "./components/AddComponent";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-      </View>
-    );
+    return <AddComponent />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
--- components/AddComponent.js
+++ components/AddComponent.js
@@ -5,33 +5,81 @@ import {
   TouchableOpacity,
   StyleSheet,
   TextInput,
-  Dimensions
+  Dimensions,
+  Button,
+  Alert
 } from "react-native";
+import PopupDialog, { SlideAnimation } from "react-native-popup-dialog";
 
 const { width, height } = Dimensions.get("window");
 
 class AddComponent extends Component {
+  state = {
+    newText: ""
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.formRow}>
-          <TextInput
-            placeholder={"Enter"}
-            style={styles.input}
-            placeholderTextColor={"#888"}
-            autoCapitalize={"none"}
-            autoCorrect={false}
-          />
-        </View>
+        <Button
+          title="Insert Button"
+          onPress={() => {
+            this.popupDialog.show();
+          }}
+        />
 
-        <TouchableOpacity>
-          <View style={styles.uploadBtn}>
-            <Text style={styles.uploadText}>Enter Text</Text>
+        <PopupDialog
+          ref={popupDialog => {
+            this.popupDialog = popupDialog;
+          }}
+          dialogAnimation={slideAnimation}
+          width={250}
+          height={150}
+          justifyContent={"center"}
+        >
+          <View style={styles.formRow}>
+            <TextInput
+              value={this.state.newText}
+              placeholder={"Input"}
+              style={styles.input}
+              placeholderTextColor={"#888"}
+              autoCapitalize={"none"}
+              autoCorrect={false}
+              onChangeText={this._onTextChange}
+            />
           </View>
-        </TouchableOpacity>
+
+          <TouchableOpacity
+            onPressOut={() => {
+              {
+                this.state.newText && this.popupDialog.dismiss();
+              }
+              this._sendAction(this.state.newText);
+            }}
+          >
+            <View style={styles.uploadBtn}>
+              <Text style={styles.uploadText}>Send!</Text>
+            </View>
+          </TouchableOpacity>
+        </PopupDialog>
       </View>
     );
   }
+
+  _sendAction = text => {
+    if (text === "") {
+      Alert.alert("Field required!");
+      return;
+    }
+
+    console.log("sending request! " + text);
+  };
+
+  _onTextChange = text => {
+    this.setState({
+      newText: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -63,7 +111,14 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "600",
     textAlign: "center"
+  },
+  dialog: {
+    justifyContent: "center"
   }
+});
+
+const slideAnimation = new SlideAnimation({
+  slideFrom: "bottom"
 });
 
 export default AddComponent;
