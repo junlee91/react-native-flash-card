884fa71d63e340326c17729aa7a7219271f431d4
junlee91 <jlee757@uic.edu>
2018-02-24 16:14:54 -0600

    Input

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import {
+  StatusBar,
+  Modal,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert
+} from "react-native";
 import {
   Container,
   Header,
@@ -15,9 +22,23 @@ import {
   CardItem
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
+import styles from "./styles";
 import AddComponent from "./AddComponent";
 
 export default class HomeScreen extends React.Component {
+  state = {
+    modalVisible: false,
+    newText: ""
+  };
+
+  openModal() {
+    this.setState({ modalVisible: true });
+  }
+
+  closeModal() {
+    this.setState({ modalVisible: false });
+  }
+
   render() {
     return (
       <Container>
@@ -34,11 +55,65 @@ export default class HomeScreen extends React.Component {
             <Title>Home</Title>
           </Body>
           <Right>
-            <Ionicons name={"ios-add"} color={"skyblue"} size={40}/>
+            <Ionicons
+              name={"ios-add"}
+              color={"skyblue"}
+              size={40}
+              onPress={() => this.openModal()}
+            />
           </Right>
         </Header>
-        
+
         <Content padder>
+          <Modal
+            visible={this.state.modalVisible}
+            animationType={"slide"}
+            onRequestClose={() => this.closeModal()}
+          >
+            <StatusBar hidden={true} />
+            <View style={styles.modalContainer}>
+              <View style={styles.innerContainer}>
+                <View style={styles.formRow}>
+                  <TextInput
+                    value={this.state.newText}
+                    placeholder={"Input"}
+                    style={styles.input}
+                    placeholderTextColor={"#888"}
+                    autoCapitalize={"none"}
+                    autoCorrect={false}
+                    onChangeText={this._onTextChange}
+                  />
+                </View>
+                <TouchableOpacity
+                  onPressOut={() => {
+                    {
+                      this.state.newText
+                        ? this._sendAction(this.state.newText)
+                        : Alert.alert("Input field is required!");
+                    }
+                    () => {
+                      {this.state.newText && this.closeModal()}
+                    }
+                  }}
+                >
+                  <View style={styles.uploadBtn}>
+                    <Text style={styles.uploadText}>Add!</Text>
+                  </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPressOut={() => {
+                    this.closeModal();
+                  }}
+                >
+                  <View style={styles.calcelBtn}>
+                    <Text style={styles.uploadText}>Cancel!</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+
           <Card>
             <CardItem header>
               <Text>Welcome to FlashCard!!!</Text>
@@ -57,4 +132,18 @@ export default class HomeScreen extends React.Component {
       </Container>
     );
   }
+
+  _sendAction = text => {
+    if (text !== "") {
+      console.log("sending request! " + text);
+    }
+
+    // TODO: API Call (POST)
+  };
+
+  _onTextChange = text => {
+    this.setState({
+      newText: text
+    });
+  };
 }
--- components/styles.js
+++ components/styles.js
@@ -1,6 +1,58 @@
 import { StyleSheet } from 'react-native';
-export default StyleSheet.create({
+const styles = StyleSheet.create({
   header: {
     flex:1
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  },
+  formRow: {
+    flexDirection: "row",
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    padding: 15
+  },
+  input: {
+    flex: 1
+  },
+  uploadBtn: {
+    alignSelf: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    width: 150,
+    height: 50,
+    backgroundColor: "#3E99EE",
+    borderRadius: 25,
+    overflow: "hidden"
+  },
+  calcelBtn: {
+    alignSelf: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    width: 150,
+    height: 50,
+    backgroundColor: "#ee553d",
+    borderRadius: 25,
+    overflow: "hidden"
+  },
+  uploadText: {
+    color: "white",
+    fontWeight: "600",
+    textAlign: "center"
+  },
+  dialog: {
+    justifyContent: "center"
   }
 });
+
+export default styles;
