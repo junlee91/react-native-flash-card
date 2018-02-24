e0818dfa0dbe3c50be6783481e9ede14f2485a27
Seho Lim <limseho657424@gmail.com>
2018-02-24 15:39:20 -0600

    fixing main handler to create new cards and categories.

--- main_handler.py
+++ main_handler.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, \
+    url_for, flash, make_response, g
 from sqlalchemy import create_engine, asc, desc, and_
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Card
@@ -17,40 +18,114 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-import new_catagory_handler
+'''
+id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(200))
+    created_on = Column(DateTime, default=func.now())
+    created_by = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship("User")
+    card = relationship("Card")
+'''
+
+
+def _translate(aa):
+
+    return aa + "bb"
+
+
+@app.route('/newCategory', methods=['POST'])
+def newCategory():
+
+    user = session.query(User).filter_by(name="seho").one()
+
+    #name = request.form['name']
+    name = "hello"
+
+    #description = request.form['description']
+    description = _translate(name)
+    created_by = user.name
+    category_list = []
+    if name and description:
+        users = session.query(User).all()
+        for c in users:
+            lower_name = c.name.lower()
+            category_list.append(lower_name)
+        if name.lower() in category_list:
+            #flash("The %s category is already exists" % name)
+            return "Success1"
+
+        new_category = Category(
+            name=name, description=description, user_id=user.id, created_by=created_by)
+        session.add(new_category)
+        session.commit()
+        # flash('New Category %s Successfully Created' %
+        #      new_category.name)
+        return "Success2"
+    else:
+        flash("We need both name and description")
+        return "Success3"
+    return "Success4"
+
+
+'''
+class Card(Base):
+    __tablename__ = 'card'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    translated_name = Column(String(250), nullable=False)
+    #memorized = Column(Integer)
+    memorized_bool = Column(Boolean)
+    created_on = Column(DateTime, default=func.now())
+    created_by = Column(String(250), nullable=False)
+    category = relationship("Category")
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User")
+'''
 
 
 @app.route("/<int:category_id>/Card/new", methods=['GET', 'POST'])
 def new_Card(category_id):
+    user = session.query(User).filter_by(name='seho')
     category = session.query(Category).filter_by(id=category_id).one()
-    user = session.query(User).filter_by(id=get_user_id(login_session)).one()
+    print("stuff")
     if request.method == 'POST':
-        name = request.form['name']
-        category = request.form['category']
+        #name = request.form['name']
+        name = "random_hello2"
+        #category = request.form['category']
+        category = "random_category2"
+        translated_name = "I am trnaslated stuff."
         # Checks if the user input a name and a description.
-        if name and description:
-            cards = session.query(Card).filter_by(category_id=course.id)
+        print("things")
+        if name and category:
+            cards = session.query(Card).filter_by(category_id=1).all()
+            print(cards)
             name = name.lower()
+
+            print("asdfasdfasdf")
             for c in cards:
                 if c.name.lower() is name:
-                    flash("The %s card is already exists" %
-                          name)  # NEEDS TO BE DELETED
-                    return render_template('newCard.html', course=course,   # NEEDS TO BE REMOVED
-                                           description=description, name=name)
-                else:
-                    created_card = Card(name=name, description=description,
-                                        category_id=category_id, created_by=user.name,
-                                        user_id=user.id)
-                    session.add(created_card)
-                    session.commit()
-                    flash("Successfully created %s" % created_card.name)
-                    return redirect(url_for('show_cards', category_id=course.id))
+                    # flash("The %s card is already exists" %
+                    #      name)  # NEEDS TO BE DELETED
+                    return "Success1"
+
+            created_card = Card(name=name, translated_name=translated_name,
+                                category_id=category_id, created_by=user.name,
+                                user_id=user.id)
+            session.add(created_card)
+            session.commit()
+            #flash("Successfully created %s" % created_card.name)
+            return "Success2"
+
         else:
-            flash("Please input a name and a category")
-            return render_template('newCard.html', course=course,
-                                   description=description, name=name)
+            #flash("Please input a name and a category")
+            return "Success3"
     else:
-        return render_template('newCard.html', course=course)
+        return "Success4"
+
+    return "Success5"
 
 
 @app.route("/populate")
