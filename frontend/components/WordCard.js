f36e35bfd6c853a1ab298c89e156ee7c83c18f10
junlee91 <jlee757@uic.edu>
2018-02-24 22:46:49 -0600

    ActivityIndicator

--- frontend/components/WordCard.js
+++ frontend/components/WordCard.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { DeckSwiper, Container, Header, View, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Toast, Button } from 'native-base';
+import React from "react";
+import {
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
+import {
+  DeckSwiper,
+  Container,
+  Header,
+  View,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Icon,
+  Toast,
+  Button
+} from "native-base";
 
 // Dummy Test Data
 const cards = [
   {
     name: "alpaca",
-    image_url: "http://www.colosseumpretoria.com/wp-content/uploads/2017/10/livechicken_3.jpg"
+    image_url:
+      "http://www.colosseumpretoria.com/wp-content/uploads/2017/10/livechicken_3.jpg"
   },
   {
     name: "llama",
-    image_url: "https://media.mnn.com/assets/images/2017/01/cow-in-pasture.jpg.838x0_q80.jpg"
+    image_url:
+      "https://media.mnn.com/assets/images/2017/01/cow-in-pasture.jpg.838x0_q80.jpg"
   },
   {
     name: "panda",
-    image_url: "https://cdn.modernfarmer.com/wp-content/uploads/2017/12/Funny-Sheep-Facts.jpg"
-  },
+    image_url:
+      "https://cdn.modernfarmer.com/wp-content/uploads/2017/12/Funny-Sheep-Facts.jpg"
+  }
 ];
 
 export default class CardContent extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -29,24 +50,26 @@ export default class CardContent extends React.Component {
 
   tapToSwitch = () => {
     this.setState({
-      picDisplay: !this.state.picDisplay,
+      picDisplay: !this.state.picDisplay
     });
-  }
+  };
 
   picDisplayFalse = () => {
-    console.log("Clicked!")
+    console.log("Clicked!");
     this.setState({
-      picDisplay: false,
+      picDisplay: false
     });
-  }
-
-
+  };
 
   render() {
     const datas = this.props;
-    
-    if(datas.data.length === 0){
-      return <Text>Loading..</Text>
+
+    if (datas.data.length === 0) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
     }
 
     console.log(datas.data);
@@ -56,37 +79,47 @@ export default class CardContent extends React.Component {
         onSwipeRight={this.picDisplayFalse}
         onSwipeLeft={this.picDisplayFalse}
         dataSource={datas.data}
-        renderItem={item =>
+        renderItem={item => (
           <TouchableOpacity activeOpacity={1} onPress={this.tapToSwitch}>
             <Card style={styles.card}>
               <CardItem cardBody>
-
                 <Text style={styles.wordStyle}>{item.name}</Text>
-                <Image style={this.state.picDisplay ? styles.picStyle : styles.picStyle_n} source={{ uri: item.img_url }} />
-              
+                <Image
+                  style={
+                    this.state.picDisplay ? styles.picStyle : styles.picStyle_n
+                  }
+                  source={{ uri: item.img_url }}
+                />
               </CardItem>
             </Card>
           </TouchableOpacity>
-
-
-        }
+        )}
       />
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  },
   wordStyle: {
     flex: 1,
-    backgroundColor: '#f8f8f8f8',
+    backgroundColor: "#f8f8f8f8",
     zIndex: 2,
     fontSize: 30,
-    textAlign: 'center',
+    textAlign: "center",
     paddingLeft: 100,
     paddingRight: 100,
     paddingTop: 120,
-    paddingBottom: 120,
-    // overflow: 'hidden',  
+    paddingBottom: 120
+    // overflow: 'hidden',
   },
   // container: {
   // alignItems: 'stretch',
@@ -96,22 +129,22 @@ const styles = StyleSheet.create({
     // display: none,
     flex: 0,
     left: -280,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     zIndex: 3,
     // flexGrow: 1,
     width: 450,
     height: 305,
-    overflow: 'hidden',
-    resizeMode: 'contain'
+    overflow: "hidden",
+    resizeMode: "contain"
     // alignItems: 'center',
     // justifyContent: 'center',
   },
   picStyle_n: {
-    display: 'none',
+    display: "none"
   },
   card: {
     flex: 1,
-    overflow: 'hidden',
-    borderRadius: 10,
-  },
-})
+    overflow: "hidden",
+    borderRadius: 10
+  }
+});
